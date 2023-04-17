4de17d7d1f35bcf2ce763e02915fe42f08ccd79a
Andrew <andrew.suponev@gmail.com>
2023-04-17 22:31:05 +0400

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = this.alphabet;
+    const upperMessage = str.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < upperMessage.length; i++) {
+      const currentSymbol = upperMessage[i];
+
+      if (alphabet.includes(currentSymbol)) {
+        const messageCharCode = currentSymbol.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCharCode = upperKey[j % upperKey.length].charCodeAt(0) - 'A'.charCodeAt(0);
+
+        const encryptedCharCode = (messageCharCode + keyCharCode) % 26;
+
+        const encryptedSymbol = alphabet[encryptedCharCode];
+
+        result += encryptedSymbol;
+
+        j++;
+      } else {
+        result += currentSymbol;
+      }
+    }
+
+    return this.type ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = this.alphabet;
+    const upperMessage = str.toUpperCase();
+    const upperKey = key.toUpperCase();
+    let result = '';
+
+    for (let i = 0, j = 0; i < upperMessage.length; i++) {
+      const currentSymbol = upperMessage[i];
+
+      if (alphabet.includes(currentSymbol)) {
+        const messageCharCode = currentSymbol.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCharCode = upperKey[j % upperKey.length].charCodeAt(0) - 'A'.charCodeAt(0);
+
+        const decryptedCharCode = (messageCharCode - keyCharCode + 26) % 26;
+
+        const decryptedSymbol = alphabet[decryptedCharCode];
+
+        result += decryptedSymbol;
+
+        j++;
+      } else {
+        result += currentSymbol;
+      }
+    }
+
+    return this.type ? result : result.split('').reverse().join('');
   }
 }
 
